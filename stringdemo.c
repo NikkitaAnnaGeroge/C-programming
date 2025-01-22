264604a928f879fb65a51642a47024fb18d8082c
NikkitaAnnaGeroge <nikkitaannageorge2028@cs.sjcetpalai.ac.in>
2025-01-22 16:27:07 +0530

    Add files via upload

---
+++ stringdemo.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+int main(){
+
+printf("size of integer=%ld",sizeof(int));
+printf("\n size of short integer=%ld",sizeof(short int));
+printf("\n size of longest integer=%ld",sizeof(long int));
+printf("\n size of float=%ld",sizeof(float));
+printf("\n size of char=%ld",sizeof(char));
+
+return 0;
+}
+
