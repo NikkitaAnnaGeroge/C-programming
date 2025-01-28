31028c45c5ec6a70a7712ceb26174a9f758c4b6a
NikkitaAnnaGeroge <nikkitaannageorge2028@cs.sjcetpalai.ac.in>
2025-01-28 22:26:07 +0530

    lowerCharacter.c

--- character.c
+++ character.c
@@ -1,9 +1,16 @@
-#include <stdio.h>
+/* 
+    To print a character in lower case
+    Author: Nikkita Anna George
+    Date  : 22-01-2025
+*/    
+
+
+#include<stdio.h>
 int main(){
- char value;
- printf("\n Enter a character in capital letter:");
- value=getchar();
- value=value+32;
- printf("lower case letter:%c\n",value);
- return 0;
+    char value;
+    printf("\n Enter a character in capital letter:");
+    value=getchar();
+    value=value+32;
+    printf("lower case letter:%c\n",value);
+    return 0;
 }
