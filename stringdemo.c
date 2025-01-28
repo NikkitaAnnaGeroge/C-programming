3d76e42ce087a5b09f134e0a46b62a88c0d1941d
NikkitaAnnaGeroge <nikkitaannageorge2028@cs.sjcetpalai.ac.in>
2025-01-28 22:38:11 +0530

    stringdemo.c

--- stringdemo.c
+++ stringdemo.c
@@ -1,12 +1,16 @@
+/*
+    To find size of integer,float and char type data
+    Author : Nikkita Anna George
+    Date   : 22-01-2025
+
+*/
 #include <stdio.h>
 int main(){
+    printf("size of integer=%ld",sizeof(int));
+    printf("\n size of short integer=%ld",sizeof(short int));
+    printf("\n size of longest integer=%ld",sizeof(long int));
+    printf("\n size of float=%ld",sizeof(float));
+    printf("\n size of char=%ld",sizeof(char));
 
-printf("size of integer=%ld",sizeof(int));
-printf("\n size of short integer=%ld",sizeof(short int));
-printf("\n size of longest integer=%ld",sizeof(long int));
-printf("\n size of float=%ld",sizeof(float));
-printf("\n size of char=%ld",sizeof(char));
-
-return 0;
+    return 0;
 }
-
