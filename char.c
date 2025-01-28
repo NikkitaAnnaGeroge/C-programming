f81ec5ce90b0ded178cf6d14d38e86fa54a46485
NikkitaAnnaGeroge <nikkitaannageorge2028@cs.sjcetpalai.ac.in>
2025-01-28 22:21:55 +0530

    char.c

--- char.c
+++ char.c
@@ -1,9 +1,18 @@
+/* 
+    To print a character
+    Author: Nikkita Anna George
+    Date  : 22-01-2025
+*/    
+
+
+
 #include<stdio.h>
 int main(){
- char value;
- //scanf("%c",&value);
- value=getchar();
- printf("%c\n",value);
- return 0;
- }
+    char value;
+    printf("Enter a character:");
+    value=getchar();
+    printf("You entered:%c\n",value);
+    return 0;
+ 
+}
  
