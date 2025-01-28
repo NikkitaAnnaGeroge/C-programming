f60fca23f70c949dc259956bb88d70c8520df558
NikkitaAnnaGeroge <nikkitaannageorge2028@cs.sjcetpalai.ac.in>
2025-01-28 22:55:08 +0530

    sum.c

--- sum.c
+++ sum.c
@@ -1,6 +1,17 @@
-#include <stdio.h>
+/*
+    To find the sum of two integers
+    Author : Nikkita Anna George
+    Date   : 22-01-2025
+
+*/
+#include<stdio.h>
 int main(){
-    printf(
-    }
-    
-    
+    int num1,num2,sum;
+    printf("\nEnter the first number:");
+    scanf("%d",&num1);
+    printf("\nEnter the second number:");
+    scanf("%d",&num2);
+    sum = num1 + num2;
+    printf("\nThe sum of %d and %d = %d",num1,num2,sum);
+    return 0;
+}
