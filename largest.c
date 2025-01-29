e27d6cad79f81f8216e80d754a73f9e680b28d4e
NikkitaAnnaGeroge <nikkitaannageorge2028@cs.sjcetpalai.ac.in>
2025-01-29 14:42:53 +0530

    Add files via upload

---
+++ largest.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+int main(){
+ int num1,num2,num3;
+ printf("/n enter three numbers");
+ scanf("%d%d%d",&num1,&num2,&num3);
+ if (num1>num2){
+    if (num1>num3){
+       printf("num 1 is greater");
+ }}else if (num2>num1 && num2>num3){
+ printf("num 2 is greater");
+ }else{
+ printf("num3 is greater");
+ }
+return 0;
+}
+
+
+
+
+
