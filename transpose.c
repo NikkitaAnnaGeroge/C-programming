d01c382db0c9412dbdb0933451c688e019f11a4e
NikkitaAnnaGeroge <nikkitaannageorge2028@cs.sjcetpalai.ac.in>
2025-02-07 10:46:47 +0530

    Add files via upload

---
+++ transpose.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+int main(){
+  int row,coloumn;
+  printf("\n Enter the  row and coloumn");
+  scanf("%d%d",&row,&coloumn);
+  int m1[row][coloumn],m2[row][coloumn],m3[row][coloumn];
+  printf("\n Enter the first matrix");
+  for (int i=0;i<row;i++){
+     for(int j=0;j<coloumn;j++){
+   scanf("%d",&m1[i][j]);
+    }
+  }
+  for (int i=0;i<coloumn;i++){
+    for(int j=0;j<row;j++0{
+  }  
+ }    
+ printf("%d"/t,&m1[j][i]);
+ return 0;
+
+ }
+
+
+
