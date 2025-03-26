7680220c48106a45b64aa2ed953b46cb9eb548a0
NikkitaAnnaGeroge <nikkitaannageorge2028@cs.sjcetpalai.ac.in>
2025-03-26 15:05:57 +0530

    Add files via upload

---
+++ sum array.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+int main(){
+int i,limit,sum=0;
+printf("\n Enter the num of elements");
+scanf("%d",&limit);
+int array [limit];
+printf("\n Enter the elemets);
+  for(int i=0;i<n,i++){
+   scanf("%d", & array);
+ sum=sum+array[i];
+  }
+printf("\n the sum of elements is ,%d");
+return 0;
+}
+  
+
+
